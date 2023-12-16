c9dad2c3f2670a8301181a6739ae535e3bfd891f
Photon <113899383+Just-Photon@users.noreply.github.com>
2023-12-17 00:47:32 +0100

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,6 +1,7 @@
 import json
 import time
 from googletrans import Translator, LANGUAGES
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def format_time(seconds):
     """Converts time in seconds to a more readable format (days, hours, minutes, seconds)."""
@@ -8,6 +9,17 @@ def format_time(seconds):
     hours, seconds = divmod(seconds, 3600)
     minutes, seconds = divmod(seconds, 60)
     return f"{int(days)}d {int(hours)}h {int(minutes)}m {int(seconds)}s"
+
+def translate_word(word, languages, translator):
+    translations = {}
+    for lang in languages:
+        try:
+            translated = translator.translate(word, dest=lang).text
+            translations[lang] = translated
+        except Exception as e:
+            print(f"Error translating {word} to {lang}: {e}")
+            translations[lang] = ""
+    return word, translations
 
 def translate_words(input_file, output_file, languages):
     translator = Translator()
@@ -36,35 +48,27 @@ def translate_words(input_file, output_file, languages):
     processed_count = 0
     start_time = time.time()
 
-    for word in words:
-        # Skip if the word is less than one character
-        if len(word) < 2:
-            continue
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_word = {executor.submit(translate_word, word, languages, translator): word for word in words if word not in translations}
 
-        if word not in translations:
-            translations[word] = {}
-
-        for lang in languages:
-            # Skip if the word has already been translated in this language
-            if lang in translations[word]:
-                continue
-
-            # Translate each word
+        for future in as_completed(future_to_word):
+            word = future_to_word[future]
             try:
-                translated = translator.translate(word, dest=lang).text
-                translations[word][lang] = translated
+                word, word_translations = future.result()
+                if word not in translations:
+                    translations[word] = {}
+                translations[word].update(word_translations)
+                processed_count += 1
             except Exception as e:
-                print(f"Error translating {word} to {lang}: {e}")
-                translations[word][lang] = ""
+                print(f"Error translating {word}: {e}")
 
-        processed_count += 1
-        elapsed_time = time.time() - start_time
-        avg_time_per_word = elapsed_time / processed_count
-        estimated_total_time = avg_time_per_word * total_words
-        remaining_time = estimated_total_time - elapsed_time
+            elapsed_time = time.time() - start_time
+            avg_time_per_word = elapsed_time / processed_count if processed_count else 0
+            estimated_total_time = avg_time_per_word * total_words
+            remaining_time = estimated_total_time - elapsed_time
 
-        formatted_time = format_time(remaining_time)
-        print(f"Processed {processed_count}/{total_words} words. Estimated time remaining: {formatted_time}", end='\r')
+            formatted_time = format_time(remaining_time)
+            print(f"Processed {processed_count}/{total_words} words. Estimated time remaining: {formatted_time}", end='\r')
 
     # Save translations to a JSON file
     with open(output_file, 'w', encoding='utf-8') as file:
